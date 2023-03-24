1b78ff258b79f5148a7e2871b0d7e81d2dc83b30
Angelo Solis <88774848+angelosolis@users.noreply.github.com>
2023-03-24 13:49:04 +0800

    Add files via upload

---
+++ Calendar/Calendar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+            for (int month = 1; month <= 12; month++)
+            {
+                PrintCalendarMonth(year, month);
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintCalendarMonth(int year, int month)
+        {
+            DateTime firstDayOfMonth = new DateTime(year, month, 1);
+            Console.WriteLine(firstDayOfMonth.ToString("MMMM"));
+            Console.WriteLine("Sun Mon Tue Wed Thu Fri Sat");
+            int dayOfWeek = (int)firstDayOfMonth.DayOfWeek;
+            for (int i = 0; i < dayOfWeek; i++)
+                Console.Write("    ");
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+            for (int day = 1; day <= daysInMonth; day++)
+            {
+                Console.Write("{0,3} ", day);
+                if ((day + dayOfWeek) % 7 == 0)
+                    Console.WriteLine();
+            }
+        }
+    }
+}
